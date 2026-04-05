ac06161479ae0aef168522f8496cee22086f2c5a
Daniil Privesentsev <privezentsev.d.a@edu.mirea.ru>
2026-04-06 00:20:55 +0300

    A Tour of Go: part 1 - add func with slice

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -4,15 +4,26 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
 )
+
+func randomFormat() string {
+	formats := []string {
+		"Hi, %v. Wasap!",
+		"Hello, %v !",
+		"Greetings, %v. Wasap!",
+	}
+
+	return formats[rand.Intn(len(formats))]
+}
 
 func Hello(name string) (message string, err error) {
 	if name == "" {
 		return "", errors.New("empty name")
 	}
 
-	message = fmt.Sprintf("Hi, %v. Welcome!", name)
+	message = fmt.Sprintf(randomFormat(), name)
 
 	file, err := os.OpenFile("test.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
